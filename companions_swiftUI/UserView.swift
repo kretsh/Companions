c66ab8363e04841a99c0bd432f81b8e1f8d91be7
Paul Krätsch <kretsh@gmail.com>
2023-04-17 12:06:15 +0200

    alpha 1st view

---
+++ companions_swiftUI/UserView.swift
@@ -0,0 +1,20 @@
+//
+//  UserView.swift
+//  companions_swiftUI
+//
+//  Created by PAUL KRETSH on 16.04.2023.
+//
+
+import SwiftUI
+
+struct UserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserView()
+    }
+}
---
+++ companions_swiftUI/model/AccessTokenInterface.swift
@@ -0,0 +1,8 @@
+//
+//  AccessTokenInterface.swift
+//  companions_swiftUI
+//
+//  Created by PAUL KRETSH on 16.04.2023.
+//
+
+import Foundation
